79394b1b8814c4a244ac76f7859505a31c8298db
zababurin <852gin@gmail.com>
2018-07-30 17:39:29 +0300

    Adding realtime chart updating. Work in progress

--- src/components/Graphic/Graphic.js
+++ src/components/Graphic/Graphic.js
@@ -1,14 +1,32 @@
 import React from 'react';
+// import Chart from './UpdatebleChart';
 import Chart from './Chart';
 import {getData} from "./utils"
+import * as d3 from "d3";
 
 class Graphic extends React.Component {
+    constructor(props) {
+        super(props);
+        this.simulationInterval = 1000;
+        this.simulationDuration = 20;
+        this.dataIndex = 0;
+        this.simulationEnd = d3.timeMinute.offset(
+            new Date(),
+            this.simulationDuration
+        );
+    }
+
     componentDidMount() {
         const { endPoint = 1 } = this.props;
         getData(endPoint).then(data => {
+            // this.setState({data}, this.appendRandomData())
             this.setState({data})
         })
     }
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
+
     componentWillReceiveProps(nextProps) {
         const { endPoint = 1 } = this.props;
         const { endPoint: nextEndPoint = 1 } = nextProps;
@@ -40,6 +58,35 @@ class Graphic extends React.Component {
                 return true
             }
     }
+
+    /* Just to simulate getting data from the server */
+    /* append as new candles */
+    appendRandomData = () => {
+        this.intervalId = setInterval(() => {
+            const { data } = this.state;
+            const lastBar = data[data.length - 1];
+            let dataPoint;
+            if (this.dataIndex < data.length) {
+                const newBar = data[this.dataIndex];
+                dataPoint = {
+                    ...newBar
+                };
+                this.dataIndex++;
+            } else {
+                const newBar = data[0];
+                dataPoint = {
+                    ...newBar
+                };
+                this.dataIndex = 0;
+            }
+            dataPoint.date = d3.timeDay.offset(lastBar.date, 1);
+            data.push(dataPoint);
+            this.setState({ data });
+            if (new Date() > this.simulationEnd) {
+                clearInterval(this.intervalId);
+            }
+        }, this.simulationInterval);
+    };
 
     render() {
         if (this.state == null) {
--- src/utils/sendOrder.js
+++ src/utils/sendOrder.js
@@ -1,17 +1,12 @@
-export default async function sendOrder ({rout, pairId = 1, balanceId = 0, type, price, amount}) {
-    // console.log("sendOrder", rout);
+export default async function sendOrder ({rout, pairId = 1, balanceId = 1, type, price, amount}) {
+    // console.log("sendOrder", rout, pairId, balanceId, type, price, amount);
 
-    // const promiseData = fetch(`${rout}`)
-    //     .then(response => response.json());
-    // return promiseData
         const rawResponse = await fetch(`${rout}`, {
             method: 'POST',
             headers: {
-                'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(
-                {
+            body: JSON.stringify({
                     "pairId": pairId,
                     "balanceId": balanceId,
                     "type": type,
@@ -23,5 +18,5 @@ export default async function sendOrder ({rout, pairId = 1, balanceId = 0, type,
         });
         const content = await rawResponse.json();
 
-        console.log("sendOrder:", content);
+        // console.log("sendOrder:", content);
 }
